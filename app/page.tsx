ae383959b180c93943d5661e427f8b84709b2090
jyanga11 <jyanga12@gmail.com>
2025-08-11 11:27:12 -0500

    fix loading screen

--- app/page.tsx
+++ app/page.tsx
@@ -91,7 +91,7 @@ export default function Home() {
             <span className="font-sans text-lg sm:text-2xl md:text-4xl inline-block w-48 sm:w-80 md:w-96 text-left">
               {displayText}
               <span className={`ml-1 ${showCursor ? 'opacity-100' : 'opacity-0'} transition-opacity duration-100`}>
-                <div className="inline-block w-3 h-8 sm:w-8 sm:h-8 md:w-5 md:h-10 bg-white"></div>
+                <div className="inline-block w-2 h-4 sm:w-3 sm:h-8 md:w-4 md:h-8 bg-white"></div>
               </span>
             </span>
           </h1>
