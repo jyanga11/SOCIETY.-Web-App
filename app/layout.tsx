4f34a69a453ab9b0664573f7e57d643e658843e9
jyanga11 <jyanga12@gmail.com>
2025-09-09 11:10:40 -0500

    added GA4 tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,16 +29,17 @@ export default function RootLayout({
       <head>
         <link rel="stylesheet" href="https://use.typekit.net/obq2ovg.css" />
         {/* Google Tag Manager */}
-        <Script id="gtm" strategy="beforeInteractive">
-          {`
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-PHGVNLN7'
-            ');
-          `}
-        </Script>
+            })(window,document,'script','dataLayer','GTM-PHGVNLN7');
+            `,
+          }}
+        />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
