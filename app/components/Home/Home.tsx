726cd37f403c9f94788499cde326a6d59ac74032
jyanga11 <jyanga12@gmail.com>
2026-06-03 12:13:18 -0500

    attention grab

--- app/components/Home/Home.tsx
+++ app/components/Home/Home.tsx
@@ -4,7 +4,7 @@ export const Home = () => {
   return (
     <div className="flex justify-center">
       <main>
-        <div className="relative inline-block m-12">
+        <div className="relative inline-block m-24">
           <Image
             className="dark:invert"
             src="/Society_Logo_Earth_2.0 copy.png"
@@ -13,11 +13,11 @@ export const Home = () => {
             height={100}
             priority
           />
-          <h1 className="absolute inset-0 flex items-center justify-center text-8xl font-arts-crafts-regular pointer-events-none">
+          <h1 className="absolute inset-0 flex items-center justify-center text-10xl font-arts-crafts-regular pointer-events-none">
             SoCIETY.
           </h1>
         </div>
-        <div className="text-4xl m-24">
+        <div className="text-8xl m-48">
           <h2 className="flex flex-col">
             <span className="pl-0">Uplifting <span className="font-arts-crafts-regular color-orange">culture</span> </span>
             <span className="pl-24">through </span>
