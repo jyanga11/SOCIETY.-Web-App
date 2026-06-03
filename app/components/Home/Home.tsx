0c212a411738e170356855b577bb8b3deeefd4d9
jyanga11 <jyanga12@gmail.com>
2026-06-03 13:18:54 -0500

    attention grab

--- app/components/Home/Home.tsx
+++ app/components/Home/Home.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export const Home = () => {
   return (
     <main className="flex min-h-screen w-full flex-col items-center justify-center px-6 py-16">
-      <div className="relative aspect-[5252/3723] w-full max-w-xl">
+      <div className="relative aspect-[5252/3723] w-full max-w-xl m16">
         <Image
           className="object-contain dark:invert"
           src="/Society_Logo_Earth_2.0 copy.png"
@@ -18,7 +18,7 @@ export const Home = () => {
       <div className="mt-48 w-full max-w-6xl text-center">
           <h2 className="flex flex-col text-6xl">
             <span className="pl-0">Uplifting <span className="font-arts-crafts-regular text-orange-500">culture</span> </span>
-            <span className="pl-16">through </span>
+            <span className="pl-8">through </span>
             <span className="pl-96 font-arts-crafts-regular text-purple-500">art & technology.</span>
           </h2>
       </div>
