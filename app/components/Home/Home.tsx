c24138546382a997ec5296589b9158371bbb1edf
jyanga11 <jyanga12@gmail.com>
2026-06-03 11:16:04 -0500

    Attention grab

--- app/components/Home/Home.tsx
+++ app/components/Home/Home.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export const Home = () => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <div className="items-center justify-items-center">
+      <main className="items-center">
         <Image
           className="dark:invert"
           src="/Society_Logo_Earth_2.0 copy.png"
