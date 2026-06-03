c22ba247370497f7a210386858af258c4087c466
jyanga11 <jyanga12@gmail.com>
2026-06-03 11:22:17 -0500

    attention grab

--- app/components/Home/Home.tsx
+++ app/components/Home/Home.tsx
@@ -2,17 +2,21 @@ import Image from "next/image";
 
 export const Home = () => {
   return (
-    <div className="items-center justify-items-center">
-      <main className="items-center">
-        <Image
-          className="dark:invert"
-          src="/Society_Logo_Earth_2.0 copy.png"
-          alt="SOCIETY. Earth logo"
-          width={600}
-          height={100}
-          priority
-        />
-        <h1 className="text-4xl font-arts-crafts-regular">SoCIETY.</h1>
+    <div className="flex justify-center">
+      <main>
+        <div className="relative inline-block">
+          <Image
+            className="dark:invert"
+            src="/Society_Logo_Earth_2.0 copy.png"
+            alt="SOCIETY. Earth logo"
+            width={600}
+            height={100}
+            priority
+          />
+          <h1 className="absolute inset-0 flex items-center justify-center text-4xl font-arts-crafts-regular pointer-events-none">
+            SoCIETY.
+          </h1>
+        </div>
       </main>
     </div>
   );
