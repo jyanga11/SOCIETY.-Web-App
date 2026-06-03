da734fdd44858634a4e65044c3e63472235103e1
jyanga11 <jyanga12@gmail.com>
2026-06-03 15:42:07 -0500

    video to bg

--- app/components/Home/Home.tsx
+++ app/components/Home/Home.tsx
@@ -6,7 +6,7 @@ export const Home = () => {
         aria-hidden
       >
         <video
-          className="h-full w-full object-contain"
+          className="h-full w-full object-contain grayscale"
           src="/3danimation.mp4"
           autoPlay
           loop
