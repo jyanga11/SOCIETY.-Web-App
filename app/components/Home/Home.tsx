f65338d4cf6b5e1aaa10b626c74b4fff09cb221c
jyanga11 <jyanga12@gmail.com>
2026-06-03 11:35:01 -0500

    attention grab

--- app/components/Home/Home.tsx
+++ app/components/Home/Home.tsx
@@ -13,15 +13,15 @@ export const Home = () => {
             height={100}
             priority
           />
-          <h1 className="absolute inset-0 flex items-center justify-center text-12xl font-arts-crafts-regular pointer-events-none">
+          <h1 className="absolute inset-0 flex items-center justify-center text-8xl font-arts-crafts-regular pointer-events-none">
             SoCIETY.
           </h1>
         </div>
-        <div className="text-8xl font-arts-crafts-regular">
-          <h2>
-            <span>Uplifting culture</span>
-            <span>through</span>
-            <span>art & technology.</span>
+        <div className="text-4xl font-arts-crafts-regular">
+          <h2 className="flex flex-col">
+            <span className="pl-0">Uplifting culture </span>
+            <span className="pl-24">through </span>
+            <span className="pl-48">art & technology.</span>
           </h2>
         </div>
       </main>
