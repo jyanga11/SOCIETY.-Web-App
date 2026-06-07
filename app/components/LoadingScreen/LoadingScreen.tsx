ea213d366103bc039e5241bae8b03b0ed9d021a5
jyanga11 <jyanga12@gmail.com>
2026-06-07 14:49:48 -0500

    mobile loading screen

--- app/components/LoadingScreen/LoadingScreen.tsx
+++ app/components/LoadingScreen/LoadingScreen.tsx
@@ -17,8 +17,8 @@ export const LoadingScreen = () => {
       <div className="text-center">
         <h1 className="font-semibold mb-4">
           <span ref={refs.societyTitleRef} className="font-arts-crafts-regular text-2xl sm:text-4xl md:text-6xl">SoCIETY.</span>
-          <span ref={refs.staticTextRef} className="text-lg sm:text-4xl md:text-4xl"> {staticText}</span>
-          <span ref={refs.typingTextRef} className="text-lg sm:text-4xl md:text-4xl inline-block w-48 sm:w-80 md:w-96 text-left">
+          <span ref={refs.staticTextRef} className="font-sans text-lg sm:text-4xl md:text-4xl"> {staticText}</span>
+          <span ref={refs.typingTextRef} className="font-sans text-lg sm:text-4xl md:text-4xl inline-block w-48 sm:w-80 md:w-96 text-left">
             {displayText}
             <span ref={refs.cursorRef} className={`ml-1 ${showCursor ? 'opacity-100' : 'opacity-0'} transition-opacity duration-100`}>
               <div className="inline-block w-2 h-5 sm:w-4 sm:h-8 bg-white"></div>
